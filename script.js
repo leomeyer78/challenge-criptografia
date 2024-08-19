0f82c6835b58da30e4b9310f86d719b61ef4d958
leomeyer78 <LEOMEYER_78@HOTMAIL.COM>
2024-08-19 10:07:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-function criptografarTexto(texto) {
-    return texto.replace(/e/g, "enter")
-                .replace(/i/g, "imes")
-                .replace(/a/g, "ai")
-                .replace(/o/g, "ober")
-                .replace(/u/g, "ufat");
+function encryptText(text) {
+    return text.replace(/e/, 'enter')
+               .replace(/i/, 'imes')
+               .replace(/a/, 'ai')
+               .replace(/o/, 'ober')
+               .replace(/u/, 'ufat');
 }
 
-function descriptografarTexto(texto) {
-    return texto.replace(/enter/g, "e")
-                .replace(/imes/g, "i")
-                .replace(/ai/g, "a")
-                .replace(/ober/g, "o")
-                .replace(/ufat/g, "u");
+function decryptText(text) {
+    return text.replace(/enter/, 'e')
+               .replace(/imes/, 'i')
+               .replace(/ai/, 'a')
+               .replace(/ober/, 'o')
+               .replace(/ufat/, 'u');
 }
 
-document.getElementById('executar').addEventListener('click', () => {
-    const texto = document.getElementById('texto').value;
-    const acao = document.getElementById('acao').value;
-    let resultado = '';
-
-    if (acao === 'criptografar') {
-        resultado = criptografarTexto(texto);
-    } else if (acao === 'descriptografar') {
-        resultado = descriptografarTexto(texto);
-    }
-
-    document.getElementById('resultado').value = resultado;
+document.getElementById('encryptBtn').addEventListener('click', function() {
+    let inputText = document.getElementById('inputText').value;
+    document.getElementById('outputText').value = encryptText(inputText);
 });
 
-document.getElementById('copiar').addEventListener('click', () => {
-    const resultado = document.getElementById('resultado');
-    resultado.select();
+document.getElementById('decryptBtn').addEventListener('click', function() {
+    let inputText = document.getElementById('inputText').value;
+    document.getElementById('outputText').value = decryptText(inputText);
+});
+
+document.getElementById('copyBtn').addEventListener('click', function() {
+    let outputText = document.getElementById('outputText');
+    outputText.select();
     document.execCommand('copy');
     alert('Texto copiado para a área de transferência!');
 });
